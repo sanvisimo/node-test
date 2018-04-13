68208d63c68d07f1be2175b0de9066128920a1f6
Simone Sanvito <sanvi.simo@gmail.com>
2018-04-13 13:36:18 +0200

    add project page

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
+    welcomeMessage: 'preog'
+  });
+})
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
